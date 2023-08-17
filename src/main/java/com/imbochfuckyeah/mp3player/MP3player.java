6f5be432c43583dc56861aabdad08228bbff0d03
ImBochFuckYeah <imbochfckyeah@gmail.com>
2023-08-16 20:58:51 -0600

    first commit

---
+++ src/main/java/com/imbochfuckyeah/mp3player/MP3player.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.imbochfuckyeah.mp3player;
+
+/**
+ *
+ * @author imboch
+ */
+public class MP3player {
+
+    public static void main(String[] args) {
+        MusicPlayer display = new MusicPlayer();
+        display.setVisible(true);
+        display.setLocationRelativeTo(null);
+        System.out.println("My aplication is runing!");
+    }
+}
